1d0a7f45b9bc7e5884c3588f6b9c01555cb4944c
PiciAkk <marci@dbx.hu>
2022-01-30 18:15:54 +0100

    I almost completed the README.md

--- README.md
+++ README.md
@@ -2,10 +2,54 @@
 
 MovieR is a neat program for downloading and playing Hungarian movies from the command line, using your favorite video player 
 
+*Disclaimer: the program is downloading torrent files from [nCORE](ncore.pro), which is a Hungarian-only, invitation-based torrent site*
+
 ## Installation guide
 
-*Coming soon...*
+1. Clone this GitHub repository
+
+```bash
+git clone https://github.com/PiciAkk/movier
+```
+
+2. Go to the newly cloned repo's folder
+
+```bash
+cd movier
+```
+
+3. Install the dependencies
+
+```bash
+pip install -r requirements.txt
+```
 
 ## Usage
 
-*Coming soon...*
+1. Make a config file that contains your preferences and nCORE login informations, and save it as `config.json`
+
+Here's an example `config.json` file
+
+```json
+{
+	"login": {
+		"username": "yourUsername",
+		"password": "yourPassword"
+	},
+	"mediaPlayer": "VLC"
+}
+```
+
+2. Start the app
+
+```bash
+python src/main.py
+```
+
+3. Enter a search query
+
+4. Done! You should see your favorite movies on the screen!
+
+# The idea for making the program
+
+The idea came from [notflix](https://github.com/Bugswriter/notflix) - thanks, [Bugswriter](https://github.com/Bugswriter/)
---
+++ src/main.py
@@ -0,0 +1,70 @@
+from functools import partial as _
+import os
+from art import *
+from ncoreparser import *
+import json
+
+
+class configFile:
+    def __init__(self, path): 
+        self.checkExistenceOfConfigFile(path)
+        self.configJSON = json.loads(open(path).read())
+        #self.__getattr__ = self.configJSON.__getitem__
+                
+    def checkExistenceOfConfigFile(self, path):
+        configMissingError = "Oh no! The config file is missing!" \
+            "Please create one with the name 'config.json'"
+        assert os.path.isfile("./config.json"), configMissingError    
+        
+    def __getattr__(self, item):
+        return self.configJSON[item]
+
+class streamingTorrentClient:
+    def __init__(self, path, mediaPlayer):
+        self.path = path
+        self.mediaPlayer = "--"+mediaPlayer.lower()
+    def play(self):
+        os.system(f"webtorrent {self.mediaPlayer} {self.path}")
+
+def setupMovierDir():
+    if not os.path.exists("/tmp/movier"): os.mkdir("/tmp/movier") 
+    if len(os.listdir("/tmp/movier")):
+        os.remove(f"/tmp/movier/{os.listdir('/tmp/movier')[0]}")
+
+clear = _(print, "\033[H\033[J")
+
+def renderLogo():
+    tprint("MovieR", font = "random")
+    print("Welcome to MovieR!\n")
+
+def main():
+    config = configFile("config.json")
+    
+    client = Client()
+    client.open(*config.login.values())
+    
+    client.download(
+        sorted(
+            client.search(
+                pattern = input("Enter search query!\n> "),
+                type = SearchParamType.HD_HUN,
+                number = 3,
+                sort_by = ParamSort.SEEDERS,
+                sort_order = ParamSeq.DECREASING
+            ),
+            key = (lambda torrent: torrent["size"]),
+            reverse = True
+        )[0],
+        "/tmp/movier/"
+    )
+
+    client.close()
+    
+    streamingTorrentClient(f"/tmp/movier/{os.listdir('/tmp/movier')[0]}", config.mediaPlayer).play()
+
+if __name__ == "__main__":
+    clear()
+    setupMovierDir()
+    renderLogo()
+    main()
+    clear()
